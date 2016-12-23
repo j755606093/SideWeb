233c9163cf106c7960a5a8fb770794bab9697c1a
zhouyuexie <zhouyuexie@gmail.com>
2016-12-23 18:54:59 +0800

    修改服务器接口;

--- app.js
+++ app.js
@@ -86,10 +86,7 @@ app.use(function(req, res, next) {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
+	res.send({error:"error"})
 });
 
 
